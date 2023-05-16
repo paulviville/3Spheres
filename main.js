7084da05883b5981e6c1813308c46d5321f8bc26
paulviville <paul.viville@etu.unistra.fr>
2023-05-16 20:20:13 +0200

    computes the tangent triangle to 3 spheres

--- main.js
+++ main.js
@@ -25,8 +25,53 @@ const orbit_controls = new OrbitControls(camera, renderer.domElement)
 
 
 const radius0 = 0.5;
-const radius1 = 0.35;
-const radius2 = 0.45;
+const radius1 = 0.15;
+const radius2 = 0.35;
+
+const c0 = new THREE.Vector3(0.5, 0.5, -0.3);
+const c1 = new THREE.Vector3(-0.35, 0.7, 0.3);
+const c2 = new THREE.Vector3(-0.27, -0.3, -0.47);
+
+
+const d1 = c1.clone().sub(c0);
+const d2 = c2.clone().sub(c0);
+const n1 = d1.clone().normalize();
+const n2 = d2.clone().normalize();
+const n = n1.clone().cross(n2).normalize();
+
+const thales1 = d1.length() *(radius1 / (radius0 - radius1) + 1);
+const cosAlpha = radius0 / (thales1);
+const p1 = c0.clone().addScaledVector(n1, radius0*cosAlpha);
+
+const thales2 = d2.length()*(radius2 / (radius0 - radius2) + 1);
+const cosBeta = radius0 / (thales2);
+const p2 = c0.clone().addScaledVector(n2, radius0*cosBeta);
+
+const q1 = n.clone().cross(n1);
+const q2 = n2.clone().cross(n);
+
+const originDiff = p2.clone().sub(p1);
+const t = (originDiff.x * q2.y - originDiff.y * q2.x) / (q1.x * q2.y - q1.y * q2.x);
+const p = p1.clone().addScaledVector(q1, t);
+
+const radiusP = Math.sqrt(radius0*radius0 - c0.distanceTo(p) * c0.distanceTo(p))
+
+const pointT0 = p.clone().addScaledVector(n, radiusP);
+
+const tangent = pointT0.clone().sub(c0).normalize();
+
+
+
+
+const pointT1 = c1.clone().addScaledVector(tangent, radius1)
+const pointT2 = c2.clone().addScaledVector(tangent, radius2)
+
+const radiusP1 = Math.sqrt(radius0*radius0 - c0.distanceTo(p1) * c0.distanceTo(p1))
+const radiusP2 = Math.sqrt(radius0*radius0 - c0.distanceTo(p2) * c0.distanceTo(p2))
+
+
+const vertex = new THREE.Mesh(new THREE.SphereGeometry(0.01, 32, 32), new THREE.MeshLambertMaterial({color: 0x000000}))
+
 
 const sphereGeo0 = new THREE.SphereGeometry(radius0, 64, 64);
 const sphereGeo1 = new THREE.SphereGeometry(radius1, 64, 64);
@@ -40,9 +85,9 @@ const sphere0 = new THREE.Mesh(sphereGeo0, sphereMat0);
 const sphere1 = new THREE.Mesh(sphereGeo1, sphereMat1);
 const sphere2 = new THREE.Mesh(sphereGeo2, sphereMat2);
 
-sphere0.position.set(0.5, 0.5, -0.3)
-sphere1.position.set(-0.35, 0.7, 0.3)
-sphere2.position.set(-0.27, -0.3, -0.47)
+sphere0.position.copy(c0)
+sphere1.position.copy(c1)
+sphere2.position.copy(c2)
 
 scene.add(sphere0)
 scene.add(sphere1)
@@ -59,10 +104,102 @@ geometry.vertices.push(sphere2.position.clone());
 let f = new THREE.Face3(0,1,2);
 geometry.faces.push(f);
 geometry.computeFaceNormals();
-let triangleMat = new THREE.MeshLambertMaterial({color: 0xffffff, side: THREE.DoubleSide});
+let triangleMat = new THREE.MeshLambertMaterial({color: 0x000000, side: THREE.DoubleSide, wireframe: true});
 let triangle = new THREE.Mesh(geometry, triangleMat);
 
 scene.add(triangle)
+
+const sphereP1 = vertex.clone();
+sphereP1.position.copy(p1);
+scene.add(sphereP1)
+
+const sphereP2 = vertex.clone();
+sphereP2.position.copy(p2);
+scene.add(sphereP2)
+
+
+const arrowN = new THREE.ArrowHelper(n, c0, 1, 0xff0000);
+const arrowN1 = new THREE.ArrowHelper(n1, c0, 1, 0x00ff00);
+const arrowN2 = new THREE.ArrowHelper(n2, c0, 1, 0x0000ff);
+scene.add(arrowN);
+scene.add(arrowN1);
+scene.add(arrowN2);
+
+
+
+
+const arrowQ1 = new THREE.ArrowHelper(q1, p1, radiusP1, 0x008800);
+const arrowQ2 = new THREE.ArrowHelper(q2, p2, radiusP2, 0x000088);
+scene.add(arrowQ1)
+scene.add(arrowQ2)
+
+
+const arrowP = new THREE.ArrowHelper(n, p, radiusP, 0xff0000);
+scene.add(arrowP);
+const sphereP = vertex.clone();
+sphereP.position.copy(p);
+scene.add(sphereP)
+
+const sphereT0 = vertex.clone();
+sphereT0.position.copy(pointT0);
+scene.add(sphereT0)
+
+const sphereT1 = vertex.clone();
+sphereT1.position.copy(pointT1);
+scene.add(sphereT1)
+
+const sphereT2 = vertex.clone();
+sphereT2.position.copy(pointT2);
+scene.add(sphereT2)
+
+const arrowT0 = new THREE.ArrowHelper(tangent, c0, radius0, 0x008800);
+scene.add(arrowT0)
+const arrowT1 = new THREE.ArrowHelper(tangent, c1, radius1, 0x008800);
+scene.add(arrowT1)
+const arrowT2 = new THREE.ArrowHelper(tangent, c2, radius2, 0x008800);
+scene.add(arrowT2)
+
+
+
+
+const tangentGeo = new THREE.Geometry();
+tangentGeo.vertices.push(pointT0.clone());
+tangentGeo.vertices.push(pointT1.clone());
+tangentGeo.vertices.push(pointT2.clone());
+
+let ft = new THREE.Face3(0,1,2);
+tangentGeo.faces.push(ft);
+tangentGeo.computeFaceNormals();
+let tangentMat = new THREE.MeshLambertMaterial({color: 0x000000, side: THREE.DoubleSide, wireframe: true});
+let tangenttriangle = new THREE.Mesh(tangentGeo, tangentMat);
+scene.add(tangenttriangle)
+
+
+const planeGeo = new THREE.PlaneGeometry(10, 10);
+const planeMat = new THREE.MeshLambertMaterial({color: 0xffffff, side: THREE.DoubleSide});
+const plane = new THREE.Mesh(planeGeo, planeMat);
+plane.lookAt(tangent)
+plane.position.copy(pointT1)
+// scene.add(plane)
+
+const circle1Geo = new THREE.CircleGeometry(radiusP1, 32)
+const circle1 = new THREE.Mesh(circle1Geo, tangentMat);
+circle1.lookAt(n1);
+circle1.position.copy(p1)
+scene.add(circle1);
+
+const circle2Geo = new THREE.CircleGeometry(radiusP2, 32)
+const circle2 = new THREE.Mesh(circle2Geo, tangentMat);
+circle2.lookAt(n2);
+circle2.position.copy(p2)
+scene.add(circle2);
+
+
+const tan1 = pointT1.clone().sub(pointT0);
+const tan2 = pointT2.clone().sub(pointT0);
+
+console.log(tan2.dot(tangent), tan1.dot(tangent))
+
 
 function update(t)
 {
